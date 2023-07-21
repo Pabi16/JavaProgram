a38545508fd80d1f4fd57e97437728fceca3ba64
Pabi16 <133085239+Pabi16@users.noreply.github.com>
2023-07-21 04:48:11 -0400

    Add files via upload

---
+++ string.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class string {
+    public static void main(String[] args) {
+        String str="pabitha";
+        String pabi="pabitha";
+        System.out.println(str.equals(pabi));
+
+
+        System.out.println(str.substring(2, 5));
+
+       System.out.println(str.replace('b', 'v')); 
+
+       System.out.println(str.contains("bi"));
+
+       System.out.println(str.concat("adi"));
+    }
+}

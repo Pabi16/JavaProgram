2f2ca35b719ec0c665078dc9850ae4ec61e08173
Pabi16 <133085239+Pabi16@users.noreply.github.com>
2023-07-21 04:35:42 -0400

    Add files via upload

---
+++ upperlower.java
@@ -0,0 +1,15 @@
+import java.Util.*;
+public class upperlower {
+    public static void main(String args[]){
+        Scanner obj = new Scanner(System.in);
+        char c=obj.next().charAt(0);
+        if(a>='A' && a<='Z');
+         System.out.print("uppercase");
+        else if(a>='a' && a<='z');
+         System.out.print("lowercase");
+        else
+         System.out.print("Invalid Number");
+                 
+
+    }
+}

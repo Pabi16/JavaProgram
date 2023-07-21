092d6a122681c9483fd7ad672e2ceca25113c122
Pabi16 <133085239+Pabi16@users.noreply.github.com>
2023-07-21 05:08:04 -0400

    Add files via upload

---
+++ minvalue.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class pabi 
+{  
+    public static void main(String[] args) {  
+  Scanner sc = new Scanner(System.in);
+  {
+     int n = sc.nextInt();
+     int arr[] = new int[n];
+     
+     for(int i=0;i<n;i++)
+     {
+      arr[j] = sc.nextInt();
+     }       
+     int min = arr[0]; 
+        for (int i=0;i<n;i++)
+        {  
+         
+           if(arr[j]<min)  
+               min = arr[j];  
+        }  
+        System.out.println(min)
+    }  }
+}

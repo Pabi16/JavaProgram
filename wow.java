bcf18642ba09b1db16414c4b27dfe9941a9a64fb
Pabi16 <133085239+Pabi16@users.noreply.github.com>
2023-07-21 04:34:31 -0400

    Add files via upload

---
+++ wow.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class wow {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        System.out.println("w");
+        for(int i=0;i<a;i++){
+            System.out.println("o");
+        }
+        System.out.println("w");
+    }
+    
+}

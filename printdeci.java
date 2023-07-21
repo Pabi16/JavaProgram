bde6ea4dc89ae00009aa50c5ccaf372a2fb42596
Pabi16 <133085239+Pabi16@users.noreply.github.com>
2023-07-21 04:58:34 -0400

    Add files via upload

---
+++ printdeci.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.lang.*;
+public class printdeci {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a=Math.Pow(2.4);
+        int b=Math.Sqrt()
+    }
+    
+}

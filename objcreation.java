9323c6ed1032a9eba0e78c0aadeab249de940174
Pabi16 <133085239+Pabi16@users.noreply.github.com>
2023-07-21 05:05:38 -0400

    Add files via upload

---
+++ objcreation.java
@@ -0,0 +1,11 @@
+ import java.util.*;
+ class pabi{
+    int n;
+ }
+ public class objcreation{
+ public static void main(String[] args) {
+    pabi sc=new pabi();
+    int n=5;
+    System.out.println(sc.n);
+ }
+}

3ce443196c2f37ec6b7558633a028b3d57850262
Pabi16 <133085239+Pabi16@users.noreply.github.com>
2023-07-21 06:31:30 -0400

    Add files via upload

---
+++ lvl2.java
@@ -0,0 +1,15 @@
+class pabi
+{
+    public static void main(String args[])
+    {
+        int n=7;
+        for(int i=1;i<=n;i++)
+        {
+            if(i%2!=0)
+            {
+                System.out.println(i + " ");
+            }
+              
+        }
+    }
+}

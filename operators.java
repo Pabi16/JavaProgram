843618b90d965a807b558f7b601d2246ea5eca32
Pabi16 <133085239+Pabi16@users.noreply.github.com>
2023-07-21 05:04:22 -0400

    Add files via upload

---
+++ operators.java
@@ -0,0 +1,8 @@
+class Example
+{
+public static void main(String args[])
+{
+int a=10,b=20;
+System.out.println("Add: " + (a+b) + " Sub: " + (a-b) + " Multiply: " + (a*b) + " Division: " + (a/b));
+}
+}

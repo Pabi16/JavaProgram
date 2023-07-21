6ef29332cdccdeb4a854c1c54adb7b20ff07136c
Pabi16 <133085239+Pabi16@users.noreply.github.com>
2023-07-21 05:04:54 -0400

    Add files via upload

---
+++ odd.java
@@ -0,0 +1,12 @@
+class pabi
+{
+    public static void main(String args[])
+    {
+       int n=5;
+        for(int i=1;i<=n;i++)
+        {
+            if(i%2!=0)
+              System.out.print(i +" ");
+        }
+    }
+}
